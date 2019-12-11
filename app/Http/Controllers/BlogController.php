a8ff436c0bb2738a3ee1b3da23af62edcafb8150
kapkam <adrinro@gmail.com>
2019-12-11 09:37:13 +0100

    #4 mostrar categories

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class BlogController extends Controller
 {
     public function index(){
       $posts = Post::all();
-      return view('welcome')->with('posts',$posts);
+      $categories = Category::all();
+
+      return view('welcome',['posts'=>$posts,'categories'=>$categories]);
     }
 }
