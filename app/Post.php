02ea4a523da78ab8e09c74b8fe6c41cc26ce42e2
kapkam <adrinro@gmail.com>
2019-12-11 09:14:58 +0100

    #3 controller Post y vista dínamica

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  protected $table='posts';
+  protected $fillable=['id','title','excerpt', 'body', 'image', 'published_at', 'category_id', 'user_id', 'created_at', 'updated_at'];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'BlogController@index')->name('welcome');
+// Route::get('/home', 'HomeController@index')->name('home');
